c41631ee09f7e532fef0943fc34054f44a5e1a3b
PIOTR\piotr <zakpiotr94@gmail.com>
2018-08-09 21:53:45 +0200

    init

---
+++ src/main/java/pl/teambride/mvp/MvpApplication.java
@@ -0,0 +1,12 @@
+package pl.teambride.mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/teambride/mvp/MvpApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.teambride.mvp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
